0089d43acc0b49b748dec7d2ac7991855b183d91
Armin Ildermi <aildermi@gmail.com>
2023-04-14 14:00:38 +0330

    refactor

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 'use strict';
 
-var Service;
-var Characteristic;
-var net = require('net');
+let Service, Characteristic;
+import { Socket } from 'net';
 var clients = {};
 var responseCallback = function() {};
+var switchStates = {};
 
-module.exports = function (homebridge) {
+export default function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerAccessory('homebridge-tcpswitch', 'TcpSwitch', TcpSwitch);
 };
 
-function TcpSwitch(log, config) {
-    this.log = log;
+class TcpSwitch {
+    constructor (log, config) {
+        this.log = log;
 
-    this.name            = config.name || 'TcpSwitch';
-    this.host            = config.host;
-    this.port            = config.port || 6269;
-    this.value           = config.value || 1
-    //
-    var clientKey = this.host + ":" + this.port;
-    if (clientKey in clients)
-        this.client = clients[clientKey];
-    else {
-        this.client = clients[clientKey] = new net.Socket();
-        this.client.connect(this.port, this.host);
-        this.client.on('data', function(data) {
-            console.log(data.toString());
-            console.log(data);
-            responseCallback(data);
-        });
+        this.name                = config.name || 'TcpSwitch';
+        this.host                = config.host;
+        this.port                = config.port || 6269;
+        this.value               = config.value || 1
+        switchStates[this.value] = false;
+        //
+        var clientKey = this.host + ":" + this.port;
+        if (clientKey in clients)
+            this.client = clients[clientKey];
+        else {
+            this.client = clients[clientKey] = new Socket();
+            this.client.connect(this.port, this.host);
+            this.client.on('data', function(data) {
+                console.log(data.toString());
+                console.log(data);
+                responseCallback(data);
+            });
+        }
+
+        this.service = new Service.Switch(this.name);
     }
-}
-
-TcpSwitch.prototype = {
-
-    tcpRequest: function(host, port, value, callback) {
+    tcpRequest (value, callback) {
         responseCallback = callback
         try {
             var arr = [];
@@ -46,56 +47,32 @@ TcpSwitch.prototype = {
                 arr = [0x72, 0x31, 0x2F + value, 0x0a, 0x0a];
             this.client.write(new Uint8Array(arr)); 
         } catch (error) {
-            this.client.connect(port, host);
+            this.client.connect(this.port, this.host);
         }
-    },
-
-    setPowerState: function(targetService, powerState, callback, context) {
-        var funcContext = 'fromSetPowerState';
-        // Callback safety
-        if (context == funcContext) {
-            if (callback) {
-                callback();
-            }
-            return;
-        }
-
-        this.tcpRequest(this.host, this.port, this.value, function(result) {
-            console.log("====");
-            console.log(this.value);
-            console.log(result);
-            console.log("====");
-            var switchValue = result[1] & 0x0F;
-            var switchStatus = result[2] & 0x0F == 0x0e;
-            console.log(switchValue);
-            console.log(switchStatus);
-            console.log(targetService);
-            console.log('----');
-            targetService.getCharacteristic(Characteristic.On).setValue(switchStatus, undefined, funcContext);
-        }.bind(this));
-    },
-
-    identify: function (callback) {
-        this.log('Identify me Senpai!');
-        callback();
-    },
-
-    getServices: function () {
-        this.services = [];
-
-        var informationService = new Service.AccessoryInformation();
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, 'TcpSwitch')
-            .setCharacteristic(Characteristic.Model, 'TcpSwitch');
-        this.services.push(informationService);
-
-        var switchService = new Service.Switch(this.name);
-        switchService
-            .getCharacteristic(Characteristic.On)
-            .on('set', this.setPowerState.bind(this, switchService));
-
-        this.services.push(switchService);
-
-        return this.services;
     }
-};
+
+    getServices () {
+        const informationService = new Service.AccessoryInformation()
+            .setCharacteristic(Characteristic.Manufacturer, 'TcpSocket')
+            .setCharacteristic(Characteristic.Model, 'TcpSocket');
+
+        this.service.getCharacteristic(Characteristic.On)
+            .on('get', this.getOnCharacteristicHandler.bind(this))
+            .on('set', this.setOnCharacteristicHandler.bind(this));
+
+        return [informationService, this.service];
+    }
+
+    setOnCharacteristicHandler (value, callback) {
+        this.tcpRequest(this.value, function(result){
+            var switchValue = result[1] & 0x0F;
+            var switchState = result[2] & 0x0F == 0x0e;
+            switchStates[switchValue] = switchState;
+            callback(null);
+        });
+    }
+
+    getOnCharacteristicHandler (callback) {
+        callback(null, switchStates[this.value]);
+    }
+}
