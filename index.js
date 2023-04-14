ef7e3a716f75d2ab6cc272a620636ad4a1746c30
Armin Ildermi <aildermi@gmail.com>
2023-04-14 13:17:54 +0330

    add debug logs

--- index.js
+++ index.js
@@ -17,29 +17,31 @@ function TcpSwitch(log, config) {
     this.host            = config.host;
     this.port            = config.port || 6269;
     this.value           = config.value || 1
+
+    this.client          = new net.Socket();
+    //
+    this.client.connect(port, host);
+    this.responseCallback = function(){}
+    this.client.on('data', function(data) {
+        console.log(data);
+        this.responseCallback(data);
+    });
 }
 
 TcpSwitch.prototype = {
 
     tcpRequest: function(host, port, value, callback) {
-        var client = new net.Socket();
-        client.connect(port, host);
-        client.on('data', function(data) {
-            console.log(data[0]);
-            if (data[0] === 83) {
-                setTimeout(function(){
-                    var arr = [];
-                    if (value < 10)
-                        arr = [0x72, 0x30 + value, 0x0a, 0x0a];
-                    else
-                        arr = [0x72, 0x31, 0x2F + value, 0x0a, 0x0a];
-                    client.write(new Uint8Array(arr)); 
-                }, 500);
-            } else {
-                callback(data[1]);
-                client.destroy()
-            }
-        });
+        this.responseCallback = callback
+        try {
+            var arr = [];
+            if (value < 10)
+                arr = [0x72, 0x30 + value, 0x0a, 0x0a];
+            else
+                arr = [0x72, 0x31, 0x2F + value, 0x0a, 0x0a];
+            this.client.write(new Uint8Array(arr)); 
+        } catch (error) {
+            this.client.connect(port, host);
+        }
     },
 
     setPowerState: function(targetService, powerState, callback, context) {
@@ -57,7 +59,11 @@ TcpSwitch.prototype = {
 
 
         this.tcpRequest(this.host, this.port, this.value, function(result) {
-            // switchService.getCharacteristic(Characteristic.On).setValue(result&value==0, undefined, 'fromSetPowerState');
+            console.log("====");
+            console.log(value);
+            console.log(result);
+            console.log("====");
+            // targetService.getCharacteristic(Characteristic.On).setValue(result, undefined, funcContext);
         }.bind(this));
     },
 
